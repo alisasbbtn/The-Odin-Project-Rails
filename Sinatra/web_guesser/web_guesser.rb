0b85014b8cc2135bcb5de2e2108262327b642754
Alice <artery.aorta@gmail.com>
2017-02-19 15:19:12 +0200

    I4: add change color

--- Sinatra/web_guesser/web_guesser.rb
+++ Sinatra/web_guesser/web_guesser.rb
@@ -6,11 +6,12 @@ SECRET_NUMBER = rand(0..100)
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message }
+  color = change_color(message)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color }
 end
 
 def check_guess(guess)
-  if (guess - SECRET_NUMBER) > 5
+  if (guess - SECRET_NUMBER).abs > 5
     return "Way too high!"
   elsif (SECRET_NUMBER - guess) > 5
     return "Way too low"
@@ -22,5 +23,15 @@ def check_guess(guess)
     return "Too low!"
   elsif guess == SECRET_NUMBER
     return "Correct!"
+  end
+end
+
+def change_color(message)
+  if message.split()[0] == "Way"
+    'darkred'
+  elsif message.split()[0] == "Too"
+    'indianred'
+  else
+    'green'
   end
 end
