eef025c4ce1d5de71aa4d62694449c792959cde0
Alice <artery.aorta@gmail.com>
2017-03-23 14:40:56 +0200

    Chapter 5 Rails Tutorial

---
+++ Routes, Views, Controllers and Assets/rest-tester/app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+	def index
+		@name = "I am the Index action!"
+	end
+end
+
---
+++ Routes, Views, Controllers and Assets/rest-tester/app/script/rest_requestor.rb
@@ -0,0 +1,16 @@
+require 'rest-client'
+
+url = "http://localhost:3000/users"
+puts RestClient.get(url)
+
+url = "http://localhost:3000/users/new"
+puts RestClient.get(url)
+
+url = "http://localhost:3000/users/id/edit"
+puts RestClient.get(url)
+
+url = "http://localhost:3000/users/id"
+puts RestClient.get(url)
+
+url = "http://localhost:3000/users"
+RestClient.post(url,"")
